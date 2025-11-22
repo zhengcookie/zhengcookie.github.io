bc1988adeac37d794f250c192da83b383d27623a
zhengcookie <z2132085753@outlook.com>
2025-11-22 17:28:32 +0800

    Site updated: 2025-11-22 17:28:31

--- js/useaplayer.js
+++ js/useaplayer.js
@@ -1,20 +1,35 @@
-const ap = new APlayer({
-    container: document.getElementById('aplayer'),
-    fixed: true,
-    lrcType: 3,
-    audio: [
-        {
-            name: 'Deadman（爱坤版）',
-            artist: '木子轩 / 七七丫',
-            url: 'http://music.163.com/song/media/outer/url?id=2719626532.mp3',
-            cover: 'https://p2.music.126.net/HfwxTYIDNxgZNa-w4r6fmg==/109951170835101706.jpg?param=90y90',
-        },
-        // {
-        //     name: '花の塔',
-        //     artist: 'さユり',
-        //     url: 'http://music.163.com/song/media/outer/url?id=1956534872.mp3',
-        //     cover: 'https://p2.music.126.net/fS_5RbP_4qg-RYreqp2tGg==/109951167558017839.jpg?param=130y130',
-        //     lrc: 'https://moechun.fun/music/lrc/さユり - 花の塔.lrc'
-        // },
-    ]
+var ap;
+function initPlayer() {
+    if (!ap) {
+        ap = new APlayer({
+            container: document.getElementById('aplayer'),
+            fixed: true,
+            lrcType: 0,  // 禁用字幕
+            audio: [
+                {
+                    name: 'Deadman（爱坤版）',
+                    artist: '木子轩 / 七七丫',
+                    url: 'http://music.163.com/song/media/outer/url?id=2719626532.mp3',
+                    cover: 'https://p2.music.126.net/HfwxTYIDNxgZNa-w4r6fmg==/109951170835101706.jpg?param=90y90',
+                },
+            ]
+        });
+    }
+}
+
+// 页面加载时初始化播放器
+window.onload = initPlayer;
+// 保存播放进度
+ap.on('play', function () {
+    localStorage.setItem('currentTime', ap.audio.currentTime);
 });
+
+// 恢复播放进度
+window.onload = function() {
+    if (localStorage.getItem('currentTime')) {
+        ap.audio.currentTime = localStorage.getItem('currentTime');
+    }
+};
+window.onbeforeunload = function() {
+    localStorage.setItem('currentTime', ap.audio.currentTime);
+};
