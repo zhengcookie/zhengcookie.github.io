abb91792b1a4c2cf9fe51e916268879f92226174
zhengcookie <z2132085753@outlook.com>
2025-11-25 01:22:53 +0800

    Site updated: 2025-11-25 01:22:52

--- js/useaplayer.js
+++ js/useaplayer.js
@@ -26,10 +26,22 @@ function initPlayer() {
                     cover:"https://p2.music.126.net/U7pqYhbbgxjPx1NYUGOGQQ==/109951165958551789.jpg?param=90y90",
                 },
                 {
-                    name:"you are not alone",
+                    name:"You Are Not Alone",
                     artist:"Michael Jackson",
                     url:"http://music.163.com/song/media/outer/url?id=27674443.mp3",
                     cover:"https://p2.music.126.net/1d4ohVbbsL6sPLBVNF8xtA==/6636652185905875.jpg?param=90y90",
+                },
+                {
+                    name:"We Are The World(Demo)",
+                    artist:"Michael Jackson",
+                    url:"http://music.163.com/song/media/outer/url?id=27505451.mp3",
+                    cover:"https://p2.music.126.net/nGFJF1DBUNUSvGHLkub8YQ==/2535473814012575.jpg?param=90y90",
+                }
+                {
+                    name:"Man In The Mirror",
+                    artist:"Michael Jackson",
+                    url:"http://music.163.com/song/media/outer/url?id=1699483.mp3",
+                    cover:"https://p2.music.126.net/PpasATnY_8tvtkHAjNQMcg==/109951163016449902.jpg?param=90y90",
                 }
             ]
         });
